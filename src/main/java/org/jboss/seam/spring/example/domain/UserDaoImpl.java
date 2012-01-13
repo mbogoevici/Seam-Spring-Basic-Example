fce66f7fa404fcf0d8d27239e0193c655e605e7c
Marius Bogoevici <marius.bogoevici@gmail.com>
2012-01-13 15:53:29 -0500

    Changes for glassfish

--- src/main/java/org/jboss/seam/spring/example/domain/UserDaoImpl.java
+++ src/main/java/org/jboss/seam/spring/example/domain/UserDaoImpl.java
@@ -19,7 +19,7 @@ public class UserDaoImpl implements UserDao {
     public User getForUsername(String username) {
         try {
             Query query = entityManager
-                    .createQuery("select u from User u where u.username = ?");
+                    .createQuery("select u from User u where u.username = ?1");
             query.setParameter(1, username);
             return (User) query.getSingleResult();
         } catch (NoResultException e) {
